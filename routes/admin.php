b8f7d021080064aeed7eb18c75c5d9b917a00745
Axel Alvarez <axjavier4alvarez44@gmail.com>
2026-01-08 23:14:01 -0600

    rutas

---
+++ routes/admin.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+Route::get('/user', function (Request $request){
+
+    return $request->user();
+
+
+})->middleware('auth:sanctum');*/
+
+
+Route::get('/', function(){
+    // return view('admin.dashboard');
+    return 'Hola desde el administrador';
+})->name('dashboard');
+
+//
+Route::get('/posts', function(){
+    return 'Lista de posts';
+})->name('posts.index');
+
+
+
+
+
+
+
+
